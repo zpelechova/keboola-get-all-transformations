40298955f4e18090f8124f4ac5ea9b4525e68670
Zuzana Pelechová <zpelechova@gmail.com>
2021-10-05 12:27:01 +0200

    only change relevant fields from data

--- main.js
+++ main.js
@@ -20,6 +20,50 @@ Apify.main(async () => {
         headers: getHeaders
     })
 
-    const data = JSON.parse(getBody);
-        await Apify.pushData(data)
+    const info = JSON.parse(getBody)
+
+    const allBuckets = []
+
+    for (const i of info) {
+        const transformations = i.rows
+        const allTranformations = []
+        for (const t in transformations) {
+            const output = transformations[t].configuration.output
+            const allOutput = []
+            for (const o in output) {
+                const outputData = {
+                    destination: output[o].destination,
+                    source: output[o].source,
+                    incremental: output[o].incremental
+                }
+                allOutput.push(outputData)
+            }
+            const input = transformations[t].configuration.input
+            const allInput = []
+            for (const i in input) {
+                const inputData = {
+                    source: input[i].source,
+                    destination: input[i].destination
+                }
+                allInput.push(inputData)
+            }
+            const transData = {
+                transformationId: transformations[t].id,
+                transformationName: transformations[t].name,
+                transformationDescription: transformations[t].description,
+                transformationIsDisabled: transformations[t].isDisabled,
+                transformationQueries: transformations[t].configuration.queries
+            }
+            allTranformations.push(transData)
+        }
+        const data = {
+            bucketId: i.id,
+            bucketName: i.name,
+            bucketDescription: i.description,
+            transformations: allTranformations
+        }
+        allBuckets.push(data)
+    }
+
+    await Apify.pushData(allBuckets)
 })
